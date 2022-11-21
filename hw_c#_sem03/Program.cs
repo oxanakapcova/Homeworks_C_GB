d47cbd5b8c8b194fd9d4ea49ab0e01a030e87e9e
oxanakapcova <sakhaoks@hotmail.com>
2022-11-21 17:03:41 +0300

    decided about even number

---
+++ hw_c#_sem03/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+ является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+Console.WriteLine("please write your number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 1)
+{
+    Console.WriteLine("its not even number");
+}
+else
+{
+    Console.WriteLine("its even number");
+}
+
