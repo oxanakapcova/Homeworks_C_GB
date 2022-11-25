65e6fe881cf502092f8f7f6e3dead3cc6a10f1c3
oxanakapcova <sakhaoks@hotmail.com>
2022-11-25 19:54:41 +0300

    currency exchange

---
+++ currency_exchange/Program.cs
@@ -0,0 +1,11 @@
+﻿// Currency exchange (hardcode)
+Console.Clear();
+string Currency;
+int a;
+double b;
+Console.WriteLine("write how many rubles do You want to change: ");
+Currency = Console.ReadLine();
+a = int.Parse(Currency);
+b = (double)a / 60;
+System.Console.WriteLine(   "its " + b + " dollars");
+//Урра получилось с первого раза!!!
