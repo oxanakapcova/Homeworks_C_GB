bf8459fa121a5e3ed9b5a5c26fca60c8277a61f5
oxanakapcova <sakhaoks@hotmail.com>
2022-11-25 19:41:21 +0300

    about sum & multiplication

---
+++ task_sum_multiplication/Program.cs
@@ -0,0 +1,19 @@
+﻿// Sum of 3 numbers + multiplication
+Console.Clear();
+string math;
+int a, b, c;
+Console.WriteLine("  Please, write 1st number: ");
+math = Console.ReadLine();
+a = int.Parse(math);
+System.Console.WriteLine(   "Please, write 2nd number: ");
+math = Console.ReadLine();
+b = int.Parse(math);
+System.Console.WriteLine(   "Please, write 3rd number: ");
+math = Console.ReadLine();
+c = int.Parse(math);
+int sum;
+int multiplication;
+sum = a + b + c;
+multiplication = a * b * c;
+System.Console.WriteLine("Sum is "+ sum +";   multiplication is "+multiplication);
+//Урра!!! с первого раза из головы!!!!
