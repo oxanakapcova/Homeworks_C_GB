367f298d0752da5cfa17a95762efa46162782264
oxanakapcova <sakhaoks@hotmail.com>
2022-11-26 13:16:40 +0300

    switch & ifElse calculator

---
+++ ifElseCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿// calculator; use if ... else
+System.Console.WriteLine(   "write 1th number: ");
+double firstValue = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine(   "+, -, / or * ");
+char operand = Convert.ToChar(Console.ReadLine());
+
+System.Console.WriteLine(   "write 2ns number: ");
+double secondValue = Convert.ToDouble(Console.ReadLine());
+
+if(operand == '+')
+{
+    double result = firstValue + secondValue;
+    System.Console.WriteLine(   "Sum is "+ result);
+}
+else if(operand == '-')
+{
+    double result = firstValue - secondValue;
+    System.Console.WriteLine(   "subtraction is "+ result);
+}
+else if(operand == '/')
+{
+    double result = firstValue / secondValue;
+    System.Console.WriteLine(   "division is " + result);
+
+}
+else if(operand == '*')
+{
+    double result = firstValue * secondValue;
+    System.Console.WriteLine(   "multiplication is " + result);
+
+}
+else
+{
+    System.Console.WriteLine(   "i dont know what You want, sorry. Try again!");
+}
+//Урра! с первого раза!!!
---
+++ switch_calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿//программа калькулятора с помощью switch (+. -, /, *)
+
+System.Console.WriteLine(   "write 1th number: ");
+double firstValue = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine(   "введите оператор +, -, * или / ");
+char symbol = Convert.ToChar(Console.ReadLine());
+
+System.Console.WriteLine(   "write 2nd number: ");
+double secondValue = Convert.ToDouble(Console.ReadLine());
+
+switch  (symbol)
+{
+    case '+':
+        System.Console.WriteLine(   "производится сложение ");
+    double resultSum = firstValue + secondValue;
+    System.Console.WriteLine(   "Sum is "+ resultSum);
+        break;
+    case '-':
+        System.Console.WriteLine(   "производится вычитание ");
+    double resultSub = firstValue - secondValue;
+    System.Console.WriteLine(resultSub);
+        break;
+    case '*':
+        System.Console.WriteLine(   "производится умножение ");
+    double resultMult = firstValue * secondValue;
+    System.Console.WriteLine(resultMult);
+        break;
+    case '/':
+        System.Console.WriteLine(   "производится деление ");
+    double resultDiv = firstValue / secondValue;
+    System.Console.WriteLine(resultDiv);
+        break;
+    default:
+        System.Console.WriteLine(   "введен неправильный символ, попробуйте еще раз ");
+        break;
+}
+// не сразу разобралась с правильной последовательностью
+//но сделала все сама, немного поэксперементировав
+//Урааа!!!!
