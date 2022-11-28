56f542f80249d4179b1f694d769b4b0478f93987
oxanakapcova <sakhaoks@hotmail.com>
2022-11-28 22:42:50 +0300

    palindrome

--- palindrome/Program.cs
+++ palindrome/Program.cs
@@ -8,31 +8,21 @@ Console.WriteLine("Hello Seminar 3!");
 12821 -> да
 23432 -> да
 */
-int value;
-System.Console.WriteLine("Please, write 5numeral number: ");
-value = int.Parse(Console.ReadLine());
-if(value > 9999 && value < 100000)
-{
-    if (true)
-    {
-    for (int i = 0; i >9999 length; i--)
-    {
-        
-    }   
-    }
-    System.Console.WriteLine("Number is palindrome.");
-    
-
-    else
-    {
-        
-    
-    System.Console.WriteLine("Number is not palindrome.");
-    }
-}
-    else
-    {
-    System.Console.WriteLine("Your number is not 5numeral number. Try again!");
-    }
-
-
+System.Console.WriteLine("write 5numeral number: ");
+int value = int.Parse(Console.ReadLine());
+int fifthValue = value%10;
+value = value/10;
+int forthValue = value%10;
+value = value/10;
+int thirdValue = value%10;
+value = value/10;
+int secondValue = value%10;
+value = value/10;
+int firstValue = value%10;
+value = value/10;
+bool result = true;
+result =firstValue-fifthValue==0 && secondValue-forthValue==0;
+if (result == true)
+System.Console.WriteLine("its palindrome number");
+else
+System.Console.WriteLine("it is not palindrome number");
